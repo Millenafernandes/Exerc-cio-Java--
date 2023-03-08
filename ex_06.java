09484d6e60d691cff8555c7ffa6a7969f8a1ba11
Millena Fernandes <107659330+Millenafernandes@users.noreply.github.com>
2023-03-08 20:40:27 -0300

    Add files via upload

---
+++ ex_06.java
@@ -0,0 +1,33 @@
+package projetoJava;
+import java.util.Scanner;
+
+public class ex_06 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int v [] = new int [4];
+		
+		int i;
+		
+		
+	for(i=0; i<v.length; i++) {
+		v[i] = sc.nextInt();
+
+}
+	 for (i=0; i<4; i++) {
+		    if(v[i]%2 == 0) {
+			     
+			      System.out.println("1");} 
+		    else { 
+		    	System.out.println("-1"); }
+			    	  
+			      }
+			      }
+			     
+			     
+			    
+		    
+}
+
